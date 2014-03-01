b2da7f684240d6e128fd6d1c2ed4394a278b2c6f
ying-w <ying.w@outlook>
2014-02-28 17:09:58 -0800

    fixed parsing issue for GREATadded comment about new functionality in IGV

--- igv.R
+++ igv.R
@@ -1,5 +1,5 @@
 # Output IGV snapshots/screenshots from R
-# Author: Ying Wu daiyingw@usc.edu
+# Author: Ying Wu
 # Current version: July 2013
 #
 # Take genomic coordinates (from BED file) or GRanges objects and pass them to
@@ -13,6 +13,9 @@
 # Example: http://plindenbaum.blogspot.com/2011/07/controlling-igv-through-port-my.html
 #
 # Sometimes IGV will die after being open for too long for no reason, when that happens, this script will hang
+#
+# Note:
+# New version of bedtools has igv command that will generate a script that will dump IGV figures
 #
 # TODO:
 # need to be more robust to different delimiters
